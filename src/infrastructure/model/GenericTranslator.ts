a37d6578bdf532aff2ec72d048c8f89aa67bf3e3
EtienneLeba <etienne.leba@gmail.com>
2024-12-11 11:08:35 +0100

    remove open ai mentions in the generic translator + reduce buffer window

--- src/infrastructure/model/GenericTranslator.ts
+++ src/infrastructure/model/GenericTranslator.ts
@@ -2,7 +2,6 @@ import AssistantChat from "src/domain/context/AssistantChat.ts";
 import AssistantToolCalls from "src/domain/context/AssistantToolCalls.ts";
 import FunctionResult from "src/domain/context/FunctionResult.ts";
 import SystemChat from "src/domain/context/SystemChat.ts";
-import { OpenAI } from "openai";
 import FunctionCall from "src/domain/function/FunctionCall.ts";
 
 export default class GenericTranslator {
@@ -10,7 +9,7 @@ export default class GenericTranslator {
     conversations: Array<
       AssistantChat | AssistantToolCalls | FunctionResult | SystemChat
     >,
-  ): OpenAI.Chat.ChatCompletionMessageParam[] {
+  ) {
     const messages = [];
 
     for (const conversationItem of conversations) {
@@ -21,26 +20,24 @@ export default class GenericTranslator {
         messages.push({
           role: conversationItem.role,
           tool_calls: toolCalls,
-        } as OpenAI.Chat.ChatCompletionAssistantMessageParam);
+        });
       } else if (conversationItem instanceof FunctionResult) {
         messages.push({
           role: conversationItem.role,
           tool_call_id: conversationItem.tool_call_id,
           content: conversationItem.content,
-        } as OpenAI.Chat.ChatCompletionMessageToolCall);
+        });
       } else if (conversationItem instanceof AssistantChat || SystemChat) {
         messages.push({
           role: conversationItem.role,
           content: conversationItem.content,
-        } as OpenAI.Chat.ChatCompletionAssistantMessageParam);
+        });
       }
     }
     return messages;
   }
 
-  public translateFunctionCallsToToolCalls(
-    functionCalls: FunctionCall[],
-  ): OpenAI.Chat.ChatCompletionMessageToolCall[] {
+  public translateFunctionCallsToToolCalls(functionCalls: FunctionCall[]) {
     const toolCalls = [];
 
     for (const functionCall of functionCalls) {
@@ -51,7 +48,7 @@ export default class GenericTranslator {
           name: functionCall.name,
           arguments: JSON.stringify(functionCall.parameters),
         },
-      } as OpenAI.Chat.ChatCompletionMessageToolCall);
+      });
     }
 
     return toolCalls;
@@ -72,12 +69,12 @@ export default class GenericTranslator {
 
   public translateToolsToFunctions(toolCalls) {
     const functionCalls = [];
-    for (const openAIToolCall of toolCalls) {
+    for (const toolCall of toolCalls) {
       functionCalls.push(
         new FunctionCall(
-          openAIToolCall.id,
-          openAIToolCall.function.name,
-          JSON.parse(openAIToolCall.function.arguments),
+          toolCall.id,
+          toolCall.function.name,
+          JSON.parse(toolCall.function.arguments),
         ),
       );
     }
